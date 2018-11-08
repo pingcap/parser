aa84a573cd5d1136e3c4cd8640cca25b3d21eb9e
tiancaiamao <tiancaiamao@gmail.com>
2018-11-08 15:10:33 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+{
+	  mv go.mod1 go.mod
+	  mv go.sum1 go.sum
+	  GO111MODULE=on go test ./...
+} || {
+	  mv go.mod go.mod1
+	  mv go.sum go.sum1
+}
+
+mv go.mod go.mod1
+mv go.sum go.sum1
