d340590402f19f0ffe27058e65d1315db8d6a805
lysu <sulifx@gmail.com>
2018-11-20 01:23:55 +0800

    address comment

--- parser_example_test.go
+++ parser_example_test.go
@@ -25,13 +25,15 @@ func Example_parseSQL() {
 	// 0. make sure import parser_driver implemented by TiDB(user also can implement own driver by self).
 	// and add `import _ "github.com/pingcap/tidb/types/parser_driver"` in the head of file.
 
-	// 1. Create a parser, this is a NOT thread-safe but heavy object,
-	// it is better to reuse it in thread-safe way as possible as we can.
+	// 1. Create a parser. The parser is NOT goroutine safe and should
+	// not be shared among multiple goroutines. However, parser is also
+	// heavy, so each goroutine should reuse its own local instance if
+	// possible.
 	p := parser.New()
 
-	// 2. Parse a text SQL into AST([]ast.StmtNode)
+	// 2. Parse a text SQL into AST([]ast.StmtNode).
 	stmtNodes, err := p.Parse("select * from tbl where id = 1", "", "")
 
-	// 3. Use AST to do cool thing~
+	// 3. Use AST to do cool things.
 	fmt.Println(stmtNodes[0], err)
 }
