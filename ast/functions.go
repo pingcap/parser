a2eb5f44549dd2fe2d2f0a4b3c7e7fdbc1d77411
yu34po <yu34po123@163.com>
2018-11-21 15:22:03 +0800

    add from_tso function

--- ast/functions.go
+++ ast/functions.go
@@ -10,7 +10,6 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package ast
 
 import (
@@ -171,6 +170,7 @@ const (
 	Year             = "year"
 	YearWeek         = "yearweek"
 	LastDay          = "last_day"
+	Tso              = "from_tso"
 
 	// string functions
 	ASCII           = "ascii"
