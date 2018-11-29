a7b7ce720778960da6493450405a0d4b88cba122
leoppro <zhaoyilin@pingcap.com>
2018-11-29 20:13:21 +0800

    add error return value for Recoverable

--- ast/util.go
+++ ast/util.go
@@ -2,10 +2,6 @@ package ast
 
 import "strings"
 
-type SQLSentence struct {
-	strings.Builder
-}
-
 func WriteName(sb *strings.Builder, name string) {
 	sb.WriteString("`")
 	sb.WriteString(EscapeName(name))
