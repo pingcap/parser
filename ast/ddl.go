f237a5da82e26aef7047b7ba227ba7bcf6065a49
honestold3 <honestold3@gmail.com>
2018-12-18 18:02:14 +0800

    DropIndexStmt in ddl_test.go

--- ast/ddl.go
+++ ast/ddl.go
@@ -754,11 +754,10 @@ type DropIndexStmt struct {
 func (n *DropIndexStmt) Restore(ctx *RestoreCtx) error {
 	ctx.WriteKeyWord("DROP INDEX ")
 	if n.IfExists {
-		ctx.WriteKeyWord(" IF EXISTS")
+		ctx.WriteKeyWord("IF EXISTS ")
 	}
 	ctx.WriteName(n.IndexName)
-
-	ctx.WriteKeyWord(" ON")
+	ctx.WriteKeyWord(" ON ")
 
 	if err := n.Table.Restore(ctx); err != nil {
 		return errors.Annotate(err, "An error occurred while add index")
--- ast/ddl_test.go
+++ ast/ddl_test.go
@@ -65,14 +65,15 @@ func (ts *testDDLSuite) TestDDLVisitorCover(c *C) {
 
 func (ts *testDDLSuite) TestDropIndex(c *C) {
 	testCases := []NodeRestoreTestCase{
-		{"t1", "`t1`"},
-		{"db.`tb`", "`db`.`tb`"},
-		{"db.`tb-ttb`", "`db`.`tb-ttb`"},
-		{"`db`.`tb-ttb`", "`db`.`tb-ttb`"},
-		{"`db.TableName`", "`db.TableName`"},
+		{"drop index a on t", "DROP INDEX `a` ON `t`"},
+		{"drop index a on db.t", "DROP INDEX `a` ON `db`.`t`"},
+		{"drop index a on db.`tb-ttb`", "DROP INDEX `a` ON `db`.`tb-ttb`"},
+		{"drop index if exists a on t", "DROP INDEX IF EXISTS `a` ON `t`"},
+		{"drop index if exists a on db.t", "DROP INDEX IF EXISTS `a` ON `db`.`t`"},
+		{"drop index if exists a on db.`tb-ttb`", "DROP INDEX IF EXISTS `a` ON `db`.`tb-ttb`"},
 	}
 	extractNodeFunc := func(node Node) Node {
-		return node.(*DropIndexStmt).Table
+		return node.(*DropIndexStmt)
 	}
-	RunNodeRestoreTest(c, testCases, "DROP INDEX a ON %s", extractNodeFunc)
+	RunNodeRestoreTest(c, testCases, "%s", extractNodeFunc)
 }
