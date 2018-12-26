9231679d3f18181ab6b702366bcf43144fed27fc
AndrewDi <pandipd@outlook.com>
2018-12-26 21:38:51 +0800

    add drop view ddl action

--- model/ddl.go
+++ model/ddl.go
@@ -53,6 +53,8 @@ const (
 	ActionCreateView                   ActionType = 21
 	ActionModifyTableCharsetAndCollate ActionType = 22
 	ActionTruncateTablePartition       ActionType = 23
+	ActionDropTableOrView              ActionType = 24
+	ActionDropView                     ActionType = 25
 )
 
 // AddIndexStr is a string related to the operation of "add index".
@@ -82,6 +84,8 @@ var actionMap = map[ActionType]string{
 	ActionCreateView:                   "create view",
 	ActionModifyTableCharsetAndCollate: "modify table charset and collate",
 	ActionTruncateTablePartition:       "truncate partition",
+	ActionDropTableOrView:              "drop table or view",
+	ActionDropView:                     "drop view",
 }
 
 // String return current ddl action in string
