9fec71d160fab35f5e55a239e37a82898ea29e3b
lysu <sulifx@gmail.com>
2019-02-14 17:13:44 +0800

    add new `flush tidb_plugin [plugin-name]`

--- ast/misc.go
+++ ast/misc.go
@@ -515,6 +515,7 @@ const (
 	FlushTables
 	FlushPrivileges
 	FlushStatus
+	FlushTiDBPlugin
 )
 
 // FlushStmt is a statement to flush tables/privileges/optimizer costs and so on.
@@ -554,6 +555,8 @@ func (n *FlushStmt) Restore(ctx *RestoreCtx) error {
 		ctx.WriteKeyWord("PRIVILEGES")
 	case FlushStatus:
 		ctx.WriteKeyWord("STATUS")
+	case FlushTiDBPlugin:
+		ctx.WriteKeyWord("TIDB_PLUGIN")
 		ctx.WritePlain(" " + n.Plugin)
 	default:
 		return errors.New("Unsupported type of FlushTables")
