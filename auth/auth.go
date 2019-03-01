2db035579668f98778cc0e6fc95b1e00a7a9b2bd
Jack Yu <yusp@pingcap.com>
2019-03-01 14:19:54 +0800

    use safe String() for UserIdentity (#227)

--- auth/auth.go
+++ auth/auth.go
@@ -35,6 +35,9 @@ type UserIdentity struct {
 // String converts UserIdentity to the format user@host.
 func (user *UserIdentity) String() string {
 	// TODO: Escape username and hostname.
+	if user == nil {
+		return ""
+	}
 	return fmt.Sprintf("%s@%s", user.Username, user.Hostname)
 }
 
