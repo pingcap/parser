f4c1304538abb8c25e00ebdb0ba46bf1056e5983
qiffang <947321353@qq.com>
2019-06-19 17:52:32 +0800

    Decimal-Leftover-Digits
    Signed-off-by: qiffang <947321353@qq.com>

--- mysql/util_test.go
+++ mysql/util_test.go
@@ -4,12 +4,14 @@ import (
 	. "github.com/pingcap/check"
 )
 
-var _ = Suite(&testTypeSuite{})
+var _ = Suite(&testUtilSuite{})
 
-func (s *testTypeSuite) TestDecimalDefaultValue(c *C) {
-	len, _ := GetDefaultFieldLengthAndDecimal(TypeDecimal)
+type testUtilSuite struct{}
+
+func (s *testUtilSuite) TestDecimalDefaultValue(c *C) {
+	len, _ := GetDefaultFieldLengthAndDecimal(TypeNewDecimal)
 	c.Assert(len, Equals, 10)
 
-	len, _ = GetDefaultFieldLengthAndDecimalForCast(TypeDecimal)
+	len, _ = GetDefaultFieldLengthAndDecimalForCast(TypeNewDecimal)
 	c.Assert(len, Equals, 10)
 }
