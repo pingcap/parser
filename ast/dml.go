0cb835cf3e45894f4f12e67c5ec8ed11d183dca5
Haibin Xie <lambdax.tyler@gmail.com>
2019-08-28 17:06:49 +0900

    ast: change the order of visiting select stmt (#522)

--- ast/dml.go
+++ ast/dml.go
@@ -766,6 +766,8 @@ type SelectStmt struct {
 	IsAfterUnionDistinct bool
 	// IsInBraces indicates whether it's a stmt in brace.
 	IsInBraces bool
+	// QueryBlockOffset indicates the order of this SelectStmt if counted from left to right in the sql text.
+	QueryBlockOffset int
 }
 
 // Restore implements Node interface.
@@ -908,6 +910,14 @@ func (n *SelectStmt) Accept(v Visitor) (Node, bool) {
 		n.TableHints = newHints
 	}
 
+	if n.Fields != nil {
+		node, ok := n.Fields.Accept(v)
+		if !ok {
+			return n, false
+		}
+		n.Fields = node.(*FieldList)
+	}
+
 	if n.From != nil {
 		node, ok := n.From.Accept(v)
 		if !ok {
@@ -922,14 +932,6 @@ func (n *SelectStmt) Accept(v Visitor) (Node, bool) {
 			return n, false
 		}
 		n.Where = node.(ExprNode)
-	}
-
-	if n.Fields != nil {
-		node, ok := n.Fields.Accept(v)
-		if !ok {
-			return n, false
-		}
-		n.Fields = node.(*FieldList)
 	}
 
 	if n.GroupBy != nil {
