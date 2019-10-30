df9c364c48bf43365799693f6e38863199338601
Jack Yu <jackysp@gmail.com>
2019-10-30 11:37:06 +0800

    mysql: add some errors for TiDB (#584)

--- mysql/errcode.go
+++ mysql/errcode.go
@@ -945,6 +945,12 @@ const (
 	ErrUnsupportedReloadPlugin    = 8018
 	ErrUnsupportedReloadPluginVar = 8019
 	ErrTableLocked                = 8020
+	ErrNotExist                   = 8021
+	ErrTxnRetryable               = 8022
+	ErrCannotSetNilValue          = 8023
+	ErrInvalidTxn                 = 8024
+	ErrEntryTooLarge              = 8025
+	ErrNotImplemented             = 8026
 	ErrInfoSchemaExpired          = 8027
 	ErrInfoSchemaChanged          = 8028
 	ErrInvalidIncrementAndOffset  = 8060
