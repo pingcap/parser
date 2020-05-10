d207a9d545de1ac4bce5d4aef7d4a25ec39a9c3d
Evan Zhou <coocood@gmail.com>
2020-05-10 10:16:40 +0800

    fix go fmt

--- model/model.go
+++ model/model.go
@@ -222,12 +222,12 @@ type TableInfo struct {
 	// enabled and the primary key is not a single integer column.
 	IsCommonHandle bool `json:"is_common_handle"`
 
-	Comment     string        `json:"comment"`
-	AutoIncID   int64         `json:"auto_inc_id"`
-	AutoIdCache int64         `json:"auto_id_cache"`
-	AutoRandID  int64         `json:"auto_rand_id"`
-	MaxColumnID int64         `json:"max_col_id"`
-	MaxIndexID  int64         `json:"max_idx_id"`
+	Comment     string `json:"comment"`
+	AutoIncID   int64  `json:"auto_inc_id"`
+	AutoIdCache int64  `json:"auto_id_cache"`
+	AutoRandID  int64  `json:"auto_rand_id"`
+	MaxColumnID int64  `json:"max_col_id"`
+	MaxIndexID  int64  `json:"max_idx_id"`
 	// UpdateTS is used to record the timestamp of updating the table's schema information.
 	// These changing schema operations don't include 'truncate table' and 'rename table'.
 	UpdateTS uint64 `json:"update_timestamp"`
