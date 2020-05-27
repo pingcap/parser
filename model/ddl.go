fdf7cbdfe4a7954800ad5d4b3499a4ffb608a335
AilinKid <314806019@qq.com>
2020-05-27 19:39:58 +0800

    add alter action type
    Signed-off-by: AilinKid <314806019@qq.com>

--- model/ddl.go
+++ model/ddl.go
@@ -72,6 +72,9 @@ const (
 	ActionRebaseAutoRandomBase          ActionType = 40
 	ActionAlterIndexVisibility          ActionType = 41
 	ActionExchangeTablePartition        ActionType = 42
+	ActionAddConstraint                 ActionType = 43
+	ActionDropConstraint                ActionType = 44
+	ActionAlterConstraint               ActionType = 45
 )
 
 const (
@@ -123,6 +126,9 @@ var actionMap = map[ActionType]string{
 	ActionRebaseAutoRandomBase:          "rebase auto_random ID",
 	ActionAlterIndexVisibility:          "alter index visibility",
 	ActionExchangeTablePartition:        "exchange partition",
+	ActionAddConstraint:                 "add check constraint",
+	ActionDropConstraint:                "drop check constraint",
+	ActionAlterConstraint:               "alter check constraint",
 }
 
 // String return current ddl action in string
