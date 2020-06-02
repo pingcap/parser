510871fb078c3ddc16d2121290d0ecef7bced31f
AilinKid <314806019@qq.com>
2020-06-02 12:24:11 +0800

    .
    Signed-off-by: AilinKid <314806019@qq.com>

--- model/model.go
+++ model/model.go
@@ -212,24 +212,24 @@ type TableInfo struct {
 	Charset string `json:"charset"`
 	Collate string `json:"collate"`
 	// Columns are listed in the order in which they appear in the schema.
-	Columns         []*ColumnInfo     `json:"cols"`
-	Indices         []*IndexInfo      `json:"index_info"`
-	ForeignKeys     []*FKInfo         `json:"fk_info"`
-	Constraints     []*ConstraintInfo `json:"constraint_info"`
-	State           SchemaState       `json:"state"`
+	Columns     []*ColumnInfo     `json:"cols"`
+	Indices     []*IndexInfo      `json:"index_info"`
+	ForeignKeys []*FKInfo         `json:"fk_info"`
+	Constraints []*ConstraintInfo `json:"constraint_info"`
+	State       SchemaState       `json:"state"`
 	// PKIsHandle is true when primary key is a single integer column.
-	PKIsHandle      bool              `json:"pk_is_handle"`
+	PKIsHandle bool `json:"pk_is_handle"`
 	// IsCommonHandle is true when clustered index feature is
 	// enabled and the primary key is not a single integer column.
 	IsCommonHandle bool `json:"is_common_handle"`
 
-	Comment         string            `json:"comment"`
-	AutoIncID       int64             `json:"auto_inc_id"`
-	AutoIdCache     int64             `json:"auto_id_cache"`
-	AutoRandID      int64             `json:"auto_rand_id"`
-	MaxColumnID     int64             `json:"max_col_id"`
-	MaxIndexID      int64             `json:"max_idx_id"`
-	MaxConstraintID int64             `json:"max_cst_id"`
+	Comment         string `json:"comment"`
+	AutoIncID       int64  `json:"auto_inc_id"`
+	AutoIdCache     int64  `json:"auto_id_cache"`
+	AutoRandID      int64  `json:"auto_rand_id"`
+	MaxColumnID     int64  `json:"max_col_id"`
+	MaxIndexID      int64  `json:"max_idx_id"`
+	MaxConstraintID int64  `json:"max_cst_id"`
 	// UpdateTS is used to record the timestamp of updating the table's schema information.
 	// These changing schema operations don't include 'truncate table' and 'rename table'.
 	UpdateTS uint64 `json:"update_timestamp"`
