11a84598acea60656b7ca2cb9fdb55d134f1e7d8
zhaox1n <zhao.x1n@foxmail.com>
2020-06-04 20:03:27 +0800

    rename

--- model/ddl.go
+++ model/ddl.go
@@ -430,11 +430,11 @@ type SchemaDiff struct {
 	// OldSchemaID is the schema ID before rename table, only used by rename table DDL.
 	OldSchemaID int64 `json:"old_schema_id"`
 
-	OtherAffectedItems []*AffectedItem `json:"other_affected_items"`
+	AffectOpts []*AffectOption `json:"other_affect_options"`
 }
 
-// AffectItem is used when a ddl affects multi tables.
-type AffectedItem struct {
+// AffectOption is used when a ddl affects multi tables.
+type AffectOption struct {
 	SchemaID    int64 `json:"schema_id"`
 	TableID     int64 `json:"table_id"`
 	OldTableID  int64 `json:"old_table_id"`
