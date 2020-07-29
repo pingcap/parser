b5a528cdf2fc6c47339e192888c3ec59b7a5b338
Han Fei <hanfei19910905@gmail.com>
2020-07-29 17:44:14 +0800

    Add hint for broadcast join. (#818) (#950)
    * add hint for bc join
    
    * refine
    
    * add hint for bc join
    
    * refine
    
    * refine
    
    * refine
    
    * refine again
    
    * pass test
    
    * fix compile error
    
    * add prefer local
    
    * use bcj_local
    
    * change bc_join to broadcast_join
    
    Co-authored-by: xufei <xufeixw@mail.ustc.edu.cn>
    Co-authored-by: ichn-hu <zfhu16@fudan.edu.cn>
    
    Co-authored-by: xufei <xufeixw@mail.ustc.edu.cn>
    Co-authored-by: ichn-hu <zfhu16@fudan.edu.cn>

--- ast/misc.go
+++ ast/misc.go
@@ -2553,7 +2553,7 @@ func (n *TableOptimizerHint) Restore(ctx *format.RestoreCtx) error {
 	switch n.HintName.L {
 	case "max_execution_time":
 		ctx.WritePlainf("%d", n.HintData.(uint64))
-	case "tidb_hj", "tidb_smj", "tidb_inlj", "hash_join", "merge_join", "inl_join":
+	case "tidb_hj", "tidb_smj", "tidb_inlj", "hash_join", "merge_join", "inl_join", "broadcast_join", "broadcast_join_local":
 		for i, table := range n.Tables {
 			if i != 0 {
 				ctx.WritePlain(", ")
