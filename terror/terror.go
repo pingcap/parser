a7af66680e9ece2b6ea4cf7f00bb7eb0b24a5f0e
imtbkcat <songlingyu@pingcap.com>
2020-08-20 14:46:29 +0800

    replace terror with standard error

--- terror/terror.go
+++ terror/terror.go
@@ -14,9 +14,7 @@
 package terror
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 	"strconv"
 
 	"github.com/pingcap/errors"
@@ -50,6 +48,8 @@ const (
 
 // ErrClass represents a class of errors.
 type ErrClass int
+
+type Error = errors.Error
 
 // Error classes.
 var (
@@ -85,6 +85,7 @@ var (
 
 var errClass2Desc = make(map[ErrClass]string)
 var errCodeMap = make(map[ErrCode]*Error)
+var rfcCode2errClass = make(map[errors.RFCErrorCode]ErrClass)
 
 // RegisterErrorClass registers new error class for terror.
 func RegisterErrorClass(classCode int, desc string) ErrClass {
@@ -111,7 +112,10 @@ func (ec ErrClass) EqualClass(err error) bool {
 		return false
 	}
 	if te, ok := e.(*Error); ok {
-		return te.class == ec
+		rfcCode := te.RFCCode()
+		if class, has := rfcCode2errClass[rfcCode]; has {
+			return class == ec
+		}
 	}
 	return false
 }
@@ -133,12 +137,10 @@ func (ec ErrClass) New(code ErrCode, message string) *Error {
 		ErrClassToMySQLCodes[ec] = clsMap
 	}
 	clsMap[code] = struct{}{}
-	err := &Error{
-		class:   ec,
-		code:    code,
-		message: message,
-	}
+	var rfcCode = fmt.Sprintf("%s:%d", errClass2Desc[ec], code)
+	err := errors.Normalize(message, errors.MySQLErrorCode(int(code)), errors.RFCCodeText(rfcCode))
 	errCodeMap[code] = err
+	rfcCode2errClass[errors.RFCErrorCode(rfcCode)] = ec
 	return err
 }
 
@@ -155,162 +157,34 @@ func (ec ErrClass) NewStd(code ErrCode) *Error {
 // so it's goroutine-safe
 // and often be used to create Error came from other systems like TiKV.
 func (ec ErrClass) Synthesize(code ErrCode, message string) *Error {
-	return &Error{
-		class:   ec,
-		code:    code,
-		message: message,
-	}
-}
-
-// Error implements error interface and adds integer Class and Code, so
-// errors with different message can be compared.
-type Error struct {
-	class      ErrClass
-	code       ErrCode
-	message    string
-	workaround string
-	args       []interface{}
-	file       string
-	line       int
-}
-
-// Class returns ErrClass
-func (e *Error) Class() ErrClass {
-	return e.class
-}
-
-// Code returns ErrCode
-func (e *Error) Code() ErrCode {
-	return e.code
-}
-
-// SetWorkaround is a decorator like method which add a workaround to
-// error which is convenient for user to search.
-func (e *Error) SetWorkaround(workaround string) *Error {
-	e.workaround = workaround
-	return e
-}
-
-// MarshalJSON implements json.Marshaler interface.
-func (e *Error) MarshalJSON() ([]byte, error) {
-	return json.Marshal(&struct {
-		Class ErrClass `json:"class"`
-		Code  ErrCode  `json:"code"`
-		Msg   string   `json:"message"`
-	}{
-		Class: e.class,
-		Code:  e.code,
-		Msg:   e.getMsg(),
-	})
-}
-
-// UnmarshalJSON implements json.Unmarshaler interface.
-func (e *Error) UnmarshalJSON(data []byte) error {
-	err := &struct {
-		Class ErrClass `json:"class"`
-		Code  ErrCode  `json:"code"`
-		Msg   string   `json:"message"`
-	}{}
-
-	if err := json.Unmarshal(data, &err); err != nil {
-		return errors.Trace(err)
-	}
-
-	e.class = err.Class
-	e.code = err.Code
-	e.message = err.Msg
-	return nil
-}
-
-// Location returns the location where the error is created,
-// implements juju/errors locationer interface.
-func (e *Error) Location() (file string, line int) {
-	return e.file, e.line
-}
-
-// Error implements error interface.
-func (e *Error) Error() string {
-	return fmt.Sprintf("[%s:%d]%s", e.class, e.code, e.getMsg())
-}
-
-func (e *Error) getMsg() string {
-	if len(e.args) > 0 {
-		return fmt.Sprintf(e.message, e.args...)
-	}
-	return e.message
-}
-
-// GenWithStack generates a new *Error with the same class and code, and a new formatted message.
-func (e *Error) GenWithStack(format string, args ...interface{}) error {
-	err := *e
-	err.message = format
-	err.args = args
-	return errors.AddStack(&err)
-}
-
-// GenWithStackByArgs generates a new *Error with the same class and code, and new arguments.
-func (e *Error) GenWithStackByArgs(args ...interface{}) error {
-	err := *e
-	err.args = args
-	return errors.AddStack(&err)
-}
-
-// FastGen generates a new *Error with the same class and code, and a new formatted message.
-// This will not call runtime.Caller to get file and line.
-func (e *Error) FastGen(format string, args ...interface{}) error {
-	err := *e
-	err.message = format
-	err.args = args
-	return errors.SuspendStack(&err)
-}
-
-// FastGen generates a new *Error with the same class and code, and a new arguments.
-// This will not call runtime.Caller to get file and line.
-func (e *Error) FastGenByArgs(args ...interface{}) error {
-	err := *e
-	err.args = args
-	return errors.SuspendStack(&err)
-}
-
-// Equal checks if err is equal to e.
-func (e *Error) Equal(err error) bool {
-	originErr := errors.Cause(err)
-	if originErr == nil {
-		return false
-	}
-
-	if error(e) == originErr {
-		return true
-	}
-	inErr, ok := originErr.(*Error)
-	return ok && e.class == inErr.class && e.code == inErr.code
-}
-
-// NotEqual checks if err is not equal to e.
-func (e *Error) NotEqual(err error) bool {
-	return !e.Equal(err)
+	return errors.Normalize(message, errors.MySQLErrorCode(int(code)), errors.RFCCodeText(fmt.Sprintf("%s:%d", errClass2Desc[ec], code)))
 }
 
 // ToSQLError convert Error to mysql.SQLError.
-func (e *Error) ToSQLError() *mysql.SQLError {
-	code := e.getMySQLErrorCode()
-	return mysql.NewErrf(code, "%s", e.getMsg())
+func ToSQLError(e *Error) *mysql.SQLError {
+	code := getMySQLErrorCode(e)
+	return mysql.NewErrf(code, "%s", e.GetMsg())
 }
 
 var defaultMySQLErrorCode uint16
 
-func (e *Error) getMySQLErrorCode() uint16 {
-	codeMap, ok := ErrClassToMySQLCodes[e.class]
+func getMySQLErrorCode(e *Error) uint16 {
+	class, ok := rfcCode2errClass[e.RFCCode()]
 	if !ok {
-		log.Warn("Unknown error class", zap.Int("class", int(e.class)))
+		log.Warn("Unknown error class", zap.Int("class", int(class)))
 		return defaultMySQLErrorCode
 	}
-	_, ok = codeMap[e.code]
+	codeMap, ok := ErrClassToMySQLCodes[class]
 	if !ok {
-		log.Debug("Unknown error code", zap.Int("class", int(e.class)), zap.Int("code", int(e.code)))
+		log.Warn("Unknown error class", zap.Int("class", int(class)))
 		return defaultMySQLErrorCode
 	}
-	return uint16(e.code)
+	_, ok = codeMap[ErrCode(e.Code())]
+	if !ok {
+		log.Debug("Unknown error code", zap.Int("class", int(class)), zap.Int("code", int(e.Code())))
+		return defaultMySQLErrorCode
+	}
+	return uint16(e.Code())
 }
 
 var (
@@ -340,7 +214,7 @@ func ErrorEqual(err1, err2 error) bool {
 	te1, ok1 := e1.(*Error)
 	te2, ok2 := e2.(*Error)
 	if ok1 && ok2 {
-		return te1.class == te2.class && te1.code == te2.code
+		return te1.RFCCode() == te2.RFCCode()
 	}
 
 	return e1.Error() == e2.Error()
@@ -374,22 +248,4 @@ func Log(err error) {
 	if err != nil {
 		log.Error("encountered error", zap.Error(err), zap.Stack("stack"))
 	}
-}
-
-// ExportErrorCodeAndWorkaround is used to produce error workaround.
-func ExportErrorCodeAndWorkaround(fileName string) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	for code, e := range errCodeMap {
-		workaround := fmt.Sprintf(
-			"[error.%v]\nerror = '''%v'''\nworkaround = '''%v'''\n\n",
-			code, e.message, e.workaround)
-		_, err = file.WriteString(workaround)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
