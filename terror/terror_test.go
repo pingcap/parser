258297116c4b6b12740f18e236d1620a6c01a682
Lingyu Song <songlingyu@pingcap.com>
2020-09-11 13:40:40 +0800

    Revert "using standard error code to replace terror (#982) (#1014)" (#1019)

--- terror/terror_test.go
+++ terror/terror_test.go
@@ -65,7 +65,7 @@ func (s *testTErrorSuite) TestTError(c *C) {
 	kvErr := ClassKV.New(1062, "key already exist")
 	e := kvErr.FastGen("Duplicate entry '%d' for key 'PRIMARY'", 1)
 	c.Assert(e.Error(), Equals, "[kv:1062]Duplicate entry '1' for key 'PRIMARY'")
-	sqlErr := ToSQLError(errors.Cause(e).(*Error))
+	sqlErr := errors.Cause(e).(*Error).ToSQLError()
 	c.Assert(sqlErr.Message, Equals, "Duplicate entry '1' for key 'PRIMARY'")
 	c.Assert(sqlErr.Code, Equals, uint16(1062))
 
@@ -77,10 +77,14 @@ func (s *testTErrorSuite) TestTError(c *C) {
 }
 
 func (s *testTErrorSuite) TestJson(c *C) {
-	prevTErr := errors.Normalize("json test", errors.MySQLErrorCode(int(CodeExecResultIsEmpty)))
+	prevTErr := &Error{
+		class:   ClassTable,
+		code:    CodeExecResultIsEmpty,
+		message: "json test",
+	}
 	buf, err := json.Marshal(prevTErr)
 	c.Assert(err, IsNil)
-	var curTErr errors.Error
+	var curTErr Error
 	err = json.Unmarshal(buf, &curTErr)
 	c.Assert(err, IsNil)
 	isEqual := prevTErr.Equal(&curTErr)
