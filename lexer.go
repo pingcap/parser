f8fe39929cd129f91c505b4bc23b3fd630e973c0
ichn-hu <zfhu16@gmail.com>
2020-12-04 16:06:49 +0800

    cherry-pick

--- lexer.go
+++ lexer.go
@@ -60,6 +60,13 @@ type Scanner struct {
 	// lastKeyword records the previous keyword returned by scan().
 	// determine whether an optimizer hint should be parsed or ignored.
 	lastKeyword int
+	// lastKeyword2 records the keyword before lastKeyword, it is used
+	// to disambiguate hint after for update, which should be ignored.
+	lastKeyword2 int
+	// lastKeyword3 records the keyword before lastKeyword2, it is used
+	// to disambiguate hint after create binding for update, which should
+	// be pertained.
+	lastKeyword3 int
 
 	// hintPos records the start position of the previous optimizer hint.
 	lastHintPos Pos
@@ -128,6 +135,8 @@ func (s *Scanner) AppendError(err error) {
 func (s *Scanner) Lex(v *yySymType) int {
 	tok, pos, lit := s.scan()
 	s.lastScanOffset = pos.Offset
+	s.lastKeyword3 = s.lastKeyword2
+	s.lastKeyword2 = s.lastKeyword
 	s.lastKeyword = 0
 	v.offset = pos.Offset
 	v.ident = lit
@@ -376,8 +385,18 @@ func startWithSlash(s *Scanner) (tok int, pos Pos, lit string) {
 		// See https://dev.mysql.com/doc/refman/5.7/en/optimizer-hints.html
 		if _, ok := hintedTokens[s.lastKeyword]; ok {
 			// only recognize optimizers hints directly followed by certain
-			// keywords like SELECT, INSERT, etc.
-			isOptimizerHint = true
+			// keywords like SELECT, INSERT, etc., only a special case "FOR UPDATE" needs to be handled
+			// we will report a warning in order to match MySQL's behavior, but the hint content will be ignored
+			if s.lastKeyword2 == forKwd {
+				if s.lastKeyword3 == binding {
+					// special case of `create binding for update`
+					isOptimizerHint = true
+				} else {
+					s.warns = append(s.warns, ParseErrorWith(s.r.data(&pos), s.r.p.Line))
+				}
+			} else {
+				isOptimizerHint = true
+			}
 		}
 
 	case '*': // '/**' if the next char is '/' it would close the comment.
