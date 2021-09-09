9c6772e6c57864f06cfa135e52e452cb72165971
tangenta <tangenta@126.com>
2021-09-09 09:35:30 +0800

    mysql/charset: add mapping from charset ID to charset name

--- mysql/charset.go
+++ mysql/charset.go
@@ -15,6 +15,11 @@ package mysql
 
 import "unicode"
 
+// CharsetIDToName maps charset ID to the charset name.
+func CharsetIDToName(chsID uint8) string {
+	return charsetIDToName[chsID]
+}
+
 // CharsetNameToID maps charset name to its default collation ID.
 func CharsetNameToID(charset string) uint8 {
 	// Use quick path for TiDB to avoid access CharsetIDs map
@@ -76,6 +81,49 @@ var CharsetIDs = map[string]uint8{
 	"geostd8":  92,
 	"cp932":    95,
 	"eucjpms":  97,
+}
+
+var charsetIDToName = map[uint8]string{
+	1:"big5",
+	3:"dec8",
+	4:"cp850",
+	6:"hp8",
+	7:"koi8r",
+	Latin1DefaultCollationID:"latin1",
+	9:"latin2",
+	10:"swe7",
+	ASCIIDefaultCollationID:"ascii",
+	12:"ujis",
+	13:"sjis",
+	16:"hebrew",
+	18:"tis620",
+	19:"euckr",
+	22:"koi8u",
+	24:"gb2312",
+	25:"greek",
+	26:"cp1250",
+	28:"gbk",
+	30:"latin5",
+	32:"armscii8",
+	UTF8DefaultCollationID:"utf8",
+	35:"ucs2",
+	36:"cp866",
+	37:"keybcs2",
+	38:"macce",
+	39:"macroman",
+	40:"cp852",
+	41:"latin7",
+	UTF8MB4DefaultCollationID:"utf8mb4",
+	51:"cp1251",
+	54:"utf16",
+	56:"utf16le",
+	57:"cp1256",
+	59:"cp1257",
+	60:"utf32",
+	BinaryDefaultCollationID:"binary",
+	92:"geostd8",
+	95:"cp932",
+	97:"eucjpms",
 }
 
 // Charsets maps charset name to its default collation name.
