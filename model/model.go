9b6ba473561628aab3d28cf1ab5e5d90755c67cc
tangenta <tangenta@126.com>
2021-09-26 11:16:26 +0800

    model: add AutoRowID field to TableInfo

--- model/model.go
+++ model/model.go
@@ -289,6 +289,7 @@ type TableInfo struct {
 
 	Comment         string `json:"comment"`
 	AutoIncID       int64  `json:"auto_inc_id"`
+	AutoRowID       int64  `json:"auto_row_id"`
 	AutoIdCache     int64  `json:"auto_id_cache"`
 	AutoRandID      int64  `json:"auto_rand_id"`
 	MaxColumnID     int64  `json:"max_col_id"`
